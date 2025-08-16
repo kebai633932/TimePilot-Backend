686e4f8506ca6de8eb35e84b1c76be30c95989e0
kejinhong <3340201045@qq.com>
2025-08-16 21:48:48 +0800

    feat: 接口名称命名

---
+++ cognilearn-hub-note-manage/src/main/java/org/cxk/infrastructure/adapter/dao/INoteDao.java
@@ -0,0 +1,17 @@
+package org.cxk.infrastructure.adapter.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.cxk.infrastructure.adapter.dao.po.Folder;
+
+/**
+ * @author KJH
+ * @description
+ * @create 2025/8/16 19:29
+ */
+@Mapper
+public interface INoteDao {
+
+}
---
+++ cognilearn-hub-note-manage/src/main/java/org/cxk/service/repository/INoteRepository.java
@@ -0,0 +1,9 @@
+package org.cxk.service.repository;
+
+/**
+ * @author KJH
+ * @description
+ * @create 2025/8/16 19:31
+ */
+public interface INoteRepository {
+}
